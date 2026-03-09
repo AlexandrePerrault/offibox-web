2ab0b78231f914b3b3f53b05bd451e152785c254
Alexandre Perrault <alexandre.perrault@gmail.com>
2026-03-09 20:30:36 +0100

    Update build web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1906307015" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4178266334" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
